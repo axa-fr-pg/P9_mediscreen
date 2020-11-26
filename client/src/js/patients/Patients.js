cf09ab53d275f75d3c7227c9738d588feaf7381d
Philippe Gey <philippe.gey@axa.fr>
2020-11-26 23:28:09 +0100

    improved patients display

--- client/src/js/patients/Patients.js
+++ client/src/js/patients/Patients.js
@@ -2,99 +2,116 @@ import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import patientsApiUrl from './api';
 
-function Patients () {
+function getPatients(setPatients, setUpdateRequired, setError) {
+    axios.get(patientsApiUrl)
+        .then(response => {
+            setPatients(response.data);
+            setUpdateRequired(false);
+            if (response.data.length === 0) setError('Database is empty !');
+        })
+        .catch( error => {
+            if (error.response) {
+                setError(error.response.status + " " + error.response.data);
+            } else {
+                setError(error.message + " : check that the server is up and that the database is available !");
+            }
+        });
+}
 
-    const [error, setError] = useState('');
-    const [patients, setPatients] = useState([]);
-    const [randomVolume, setRandomVolume] = useState(5);
+function PatientsList({patients, setPatients, error, updateRequired, setUpdateRequired, setError}) {
 
     useEffect(() => {
-        axios.get(patientsApiUrl)
-            .then(response => {
-                setPatients(response.data);
-                if (response.data.length === 0) setError('Database is empty !');
-                else setError('');
-            })
-            .catch( error => {
-                    if (error.response) {
-                        setError(error.response.status + " " + error.response.data);
-                    } else {
-                        setError(error.message + " : check that the server is up and that the database is available !");
-                    }
-            });
-    }, []);
+        if (updateRequired) getPatients(setPatients, setUpdateRequired, setError);
+    });
 
-    function displayError() {
-        if (! error) return null;
-        return (
-            <footer>
-                {error}
-            </footer>
-        );
-    }
-
-    function generateRandomPatients() {
-        axios.post(patientsApiUrl+"/random/"+randomVolume)
-            .then(response => {
-                setPatients(response.data);
-                if (response.data.length === 0) setError('Could not populate database !');
-                else setError('');
-            })
-            .catch(error => {
-                if (error.response) {
-                    setError(error.response.status + " " + error.response.data);
-                } else {
-                    setError(error.message + " : check that the server is up and that the database is available !");
-                }
-            });
-    }
-
-    function displayPatients() {
-        if (patients.length === 0) return null;
-        return (
-            <nav>
-                <table>
-                    <tr>
-                        <th>Patient id</th>
-                        <th>Family name</th>
-                        <th>Date of birth</th>
+    if (patients.length === 0) return null;
+    return (
+        <nav>
+            <table>
+                <thead>
+                <tr>
+                    <th>Patient id</th>
+                    <th>Family name</th>
+                    <th>Date of birth</th>
+                </tr>
+                </thead>
+                <tbody>
+                {patients.map(patient => (
+                    <tr key={patient.id} onClick={()=>window.location.href="/patients/"+patient.id}>
+                        <td>{patient.id}</td>
+                        <td>{patient.family}</td>
+                        <td>{patient.dob}</td>
                     </tr>
-                    {patients.map(patient => (
-                        <tr onClick={()=>window.location.href="/patients/"+patient.id}>
-                            <td>{patient.id}</td>
-                            <td>{patient.family}</td>
-                            <td>{patient.dob}</td>
-                        </tr>
-                    ))}
-                </table>
-            </nav>
-        );
-    }
+                ))}
+                </tbody>
+            </table>
+        </nav>
+    );
+}
 
-    function displayRandomRequest() {
-        return (
-            <form>
-                <label>
-                    <button onClick={generateRandomPatients}>Add</button>
-                    <input className="smallest-width" defaultValue={randomVolume} onChange={onChange}/>
-                    random patients to database
-                </label>
-            </form>
-        );
-    }
+function PatientsError({error}) {
+
+    if (! error) return null;
+    return (
+        <footer>
+            {error}
+        </footer>
+    );
+}
+
+function generateRandomPatients(event, inputField, randomVolume, setUpdateRequired, setError) {
+    event.preventDefault();
+    inputField.blur();
+    setError("Processing request...");
+
+    axios.post(patientsApiUrl+"/random/"+randomVolume)
+        .then(response => {
+            setUpdateRequired(true);
+            setError(response.data.length + " random patients have been generated successfully !");
+        })
+        .catch(error => {
+            if (error.response) {
+                setError(error.response.status + " " + error.response.data);
+            } else {
+                setError(error.message + " : check that the server is up and that the database is available !");
+            }
+        });
+}
+
+function PatientsRandom({setUpdateRequired, setError}) {
+    const [randomVolume, setRandomVolume] = useState(5);
+    const [inputField, setInputField] = useState(null);
 
     function onChange (field) {
-        field.persist();
+        console.log("Saisie : ", field.target.nodeName);
         setRandomVolume(field.target.value);
+        setInputField(field.target);
     }
+
+    return (
+        <form>
+            <label>
+                <button onClick={(event) => generateRandomPatients(event, inputField, randomVolume, setUpdateRequired, setError)}>Add</button>
+                <input className="smallest-width" value={randomVolume} onChange={onChange} />
+                random patient(s) to database
+            </label>
+        </form>
+    );
+}
+
+function Patients() {
+    const [patients, setPatients] = useState([]);
+    const [error, setError] = useState('');
+    const [updateRequired, setUpdateRequired] = useState('false');
 
     return (
         <div>
             <h1>Patient list</h1>
-            {displayPatients()}
+            <PatientsList patients={patients} setPatients={setPatients} updateRequired={updateRequired}
+                          setUpdateRequired={setUpdateRequired} setError={setError} />
             <button onClick={() => window.location.href='/patients/new'}>Register new patient</button>
-            {displayRandomRequest()}
-            {displayError()}
+            <PatientsRandom setUpdateRequired={setUpdateRequired} setError={setError} />
+            <PatientsError error={error} />
         </div>
     );
 }
