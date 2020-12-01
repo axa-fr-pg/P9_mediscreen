9d706eabfd41e8c58a7d410afe7b1e6842fe2eb3
Philippe Gey <philippe.gey@axa.fr>
2020-12-01 18:39:07 +0100

    improved random generation - dob more realistic

--- patient/src/main/java/mediscreen/patient/model/PatientEntity.java
+++ patient/src/main/java/mediscreen/patient/model/PatientEntity.java
@@ -39,12 +39,12 @@ public class PatientEntity {
 
     public static PatientEntity random() {
         PatientEntity patient = new PatientEntity();
-        patient.family = randomAlphabetic(nextInt(3, 10));
-        patient.given = randomAlphabetic(nextInt(3, 10));
-        patient.dob = LocalDate.of(nextInt(1, 2019), nextInt(1, 12), nextInt(1, 28));
+        patient.family = randomAlphabetic(nextInt(3, 15));
+        patient.given = randomAlphabetic(nextInt(3, 15));
+        patient.dob = LocalDate.of(nextInt(1900, 2019), nextInt(1, 12), nextInt(1, 28));
         patient.sex = nextBoolean() ? "F" : "M";
-        patient.address = randomAlphabetic(nextInt(3, 10));
-        patient.phone = randomNumeric(nextInt(1, 3)) + "-" + randomNumeric(nextInt(2, 4)) + "-" + randomNumeric(nextInt(1, 3));
+        patient.address = randomAlphabetic(nextInt(3, 30));
+        patient.phone = randomNumeric(nextInt(1, 3)) + "-" + randomNumeric(nextInt(3, 6)) + "-" + randomNumeric(nextInt(3, 6));
         return patient;
     }
 }
