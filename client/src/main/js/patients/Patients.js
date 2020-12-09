e8a276b3600e9dde1e3b6e9c56bd2fa8aadc553a
Philippe Gey <philippe.gey@axa.fr>
2020-12-09 11:23:54 +0100

    centralized API definition

--- client/src/main/js/patients/Patients.js
+++ client/src/main/js/patients/Patients.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
-import patientsApiUrl from './patientApi';
+import {patientsApiUrl} from '../api/URLs';
 import {useHistory} from "react-router";
 
 function getPatients(setPatients, setUpdateRequired, setError) {
